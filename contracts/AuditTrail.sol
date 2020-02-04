2841dccd4e5df0a41a1342be7aeb948f3568aec4
Dario <dariolobuglio@gmail.com>
2020-02-04 12:12:41 +0100

    refactor

--- contracts/AuditTrail.sol
+++ contracts/AuditTrail.sol
@@ -4,9 +4,33 @@ pragma solidity <0.6.0;
 Gas to deploy: 1.441.547
 */
 
+import "./libraries/Utils.sol";
+
+import "./interfaces/FileInterface.sol";
 import "./interfaces/EventInterface.sol";
+import "./interfaces/EventInterface.sol";
+import "./interfaces/DocumentInterface.sol";
+import "./interfaces/SignatureInterface.sol";
+import "./interfaces/SignaturitUserInterface.sol";
 
 contract AuditTrails {
+
+    string constant private FILE_NOTIFIERS_KEY = "file-notifiers";
+    string constant private EVENT_NOTIFIERS_KEY = "event-notifiers";
+    string constant private DOCUMENT_NOTIFIERS_KEY = "document-notifiers";
+    string constant private SIGNATURE_NOTIFIERS_KEY = "signature-notifiers";
+
+    string constant private FILE_CREATED_EVENT = "file.contract.created";
+    string constant private EVENT_CREATED_EVENT = "event.contract.created";
+    string constant private TIMELOG_ADDED_EVENT = "timelog.added";
+    string constant private DOCUMENT_CREATED_EVENT = "document.contract.created";
+    string constant private SIGNATURE_CREATED_EVENT = "signature.contract.created";
+
+    string constant private DOCUMENT_SIGNED_EVENT = "document.contract.signed";
+    string constant private DOCUMENT_DECLINED_EVENT = "document.contract.declined";
+    string constant private DOCUMENT_CANCELED_EVENT = "document.contract.canceled";
+
+    string constant private VALIDATED_NOTIFIERS_KEY = "validated-notifiers";
 
     struct AuditTrail {
         address signatureAddress;
@@ -17,88 +41,28 @@ contract AuditTrails {
         uint terminatedAt;
     }
 
-    mapping(string => address) documentAddress;
-    mapping(address => AuditTrail) documentAuditTrail;
-    mapping(address => bool) admittedManagers;
+    mapping(address => mapping(string => AuditTrail)) requesterAuditTrails;
+    mapping(address => mapping(address => bool)) admittedNotifiers;
 
-    constructor(
-    ) public {
-        admittedManagers[msg.sender] = true;
-    }
+    constructor() public {}
 
-    modifier onlyManager() {
-        require(
-            admittedManagers[msg.sender] == true,
-            "Only Signaturit's manager account can perform this action"
-        );
-
-        _;
-    }
-
-    function addManager(
-        address manager
+    function subscribe(
+        address requesterSmartContract
     )
         public
-        onlyManager
     {
-        admittedManagers[manager] = true;
+        SignaturitUserInterface tmpUser = SignaturitUserInterface(requesterSmartContract);
+
+        admittedNotifiers[tmpUser.ownerAddress()][msg.sender] = true;
+
+        tmpUser.setAddressArrayAttribute(FILE_NOTIFIERS_KEY, address(this));
+        tmpUser.setAddressArrayAttribute(EVENT_NOTIFIERS_KEY, address(this));
+        tmpUser.setAddressArrayAttribute(DOCUMENT_NOTIFIERS_KEY, address(this));
     }
 
-    function createAudit(
-        string  memory hashedDocumentId,
-        address documentContract,
-        address signatureContract,
+    function getAudit(
         address requesterPubKey,
-        address signerPubKey,
-        address fileContractAddress
-    )
-        public
-        onlyManager
-    {
-        require(documentAddress[hashedDocumentId] == address(0), "The audit for this document already exists");
-
-        address[] memory eventsInAudit;
-
-        AuditTrail memory audit = AuditTrail(
-            signatureContract,
-            requesterPubKey,
-            signerPubKey,
-            fileContractAddress,
-            eventsInAudit,
-            0
-        );
-
-        documentAuditTrail[documentContract] = audit;
-
-        documentAddress[hashedDocumentId] = documentContract;
-    }
-
-    function terminateAudit(
-        uint terminatedTime,
-        string memory hashedId
-    )
-        public
-        onlyManager
-    {
-        require(documentAddress[hashedId] != address(0), "The audit for this document doesn't exists");
-
-        documentAuditTrail[documentAddress[hashedId]].terminatedAt = terminatedTime;
-    }
-
-    function addEventInAudit(
-        address eventAddress,
-        string memory hashedId
-    )
-        public
-        onlyManager
-    {
-        require(documentAddress[hashedId] != address(0), "The audit for this document doesn't exists");
-
-        documentAuditTrail[documentAddress[hashedId]].events.push(eventAddress);
-    }
-
-    function getAuditByDocumentId(
-        string memory hashedId
+        string memory documentHashedId
     )
         public
         view
@@ -112,11 +76,13 @@ contract AuditTrails {
             uint terminatedAt
         )
     {
-        AuditTrail memory tmpAudit = _getAudit(hashedId);
+        AuditTrail memory tmpAudit = _getAudit(requesterPubKey, documentHashedId);
+
+        SignatureInterface signature = SignatureInterface(tmpAudit.signatureAddress);
 
         return (
             tmpAudit.signatureAddress,
-            documentAddress[hashedId],
+            signature.getDocument(documentHashedId),
             tmpAudit.requesterAddress,
             tmpAudit.signerAddress,
             tmpAudit.fileAddress,
@@ -127,7 +93,8 @@ contract AuditTrails {
 
     function getEventInAudit(
         uint index,
-        string memory hashedId
+        address requesterPubKey,
+        string memory documentHashedId
     )
         public
         view
@@ -137,7 +104,7 @@ contract AuditTrails {
         string memory userAgent,
         uint createdAt
     ) {
-        AuditTrail memory tmpAudit = _getAudit(hashedId);
+        AuditTrail memory tmpAudit = _getAudit(requesterPubKey, documentHashedId);
 
         require(tmpAudit.events.length > index, "The index exceeds the number of events");
 
@@ -151,15 +118,123 @@ contract AuditTrails {
         );
     }
 
-    function _getAudit(
-        string memory hashedId
+    function notify (
+        string memory eventType,
+        address addr
     )
-        private
-        view
-        returns(AuditTrail memory)
+        public
     {
-        require(documentAddress[hashedId] != address(0), "The audit for this document doesn't exists");
+        bytes32 bytes32event = Utils.keccak(eventType);
 
-        return documentAuditTrail[documentAddress[hashedId]];
+        if (bytes32event == Utils.keccak(DOCUMENT_CREATED_EVENT)) {
+            DocumentInterface document = DocumentInterface(addr);
+            SignatureInterface signature = SignatureInterface(document.signature());
+
+            _checkValidCaller(address(signature.userContract()));
+
+            address owner = signature.owner();
+            string memory documentId = document.id();
+
+            require(
+                !_checkExistence(owner, documentId),
+                "The audit for this document already exists"
+            );
+
+            address[] memory eventsInAudit;
+
+            AuditTrail memory audit = AuditTrail(
+                address(signature),
+                signature.owner(),
+                document.signer(),
+                address(0),
+                eventsInAudit,
+                0
+            );
+
+            requesterAuditTrails[signature.owner()][document.id()] = audit;
+
+        } else if (bytes32event == Utils.keccak(FILE_CREATED_EVENT)) {
+            FileInterface file = FileInterface(addr);
+            DocumentInterface document = DocumentInterface(file.parent());
+            SignatureInterface signature = SignatureInterface(document.signature());
+
+            _checkValidCaller(address(signature.userContract()));
+
+            address owner = signature.owner();
+            string memory documentId = document.id();
+
+            require(_checkExistence(owner, documentId), "The audit for this document doesn't exists");
+
+            requesterAuditTrails[owner][documentId].fileAddress = addr;
+
+        } else if (bytes32event == Utils.keccak(EVENT_CREATED_EVENT)) {
+            EventInterface documentEvent = EventInterface(addr);
+            DocumentInterface document = DocumentInterface(documentEvent.parent());
+            SignatureInterface signature = SignatureInterface(document.signature());
+
+            _checkValidCaller(address(signature.userContract()));
+
+            address owner = signature.owner();
+            string memory documentId = document.id();
+
+            require(_checkExistence(owner, documentId), "The audit for this document doesn't exists");
+
+            bytes32 documentEventType = Utils.keccak(documentEvent.eventType());
+
+            if(
+                documentEventType == Utils.keccak(DOCUMENT_SIGNED_EVENT) ||
+                documentEventType == Utils.keccak(DOCUMENT_DECLINED_EVENT) ||
+                documentEventType == Utils.keccak(DOCUMENT_CANCELED_EVENT)
+            ) {
+                requesterAuditTrails[owner][documentId].terminatedAt = documentEvent.createdAt();
+            }
+
+            requesterAuditTrails[signature.owner()][document.id()].events.push(addr);
+        }
+    }
+
+    function _getAudit(address requester, string memory documentId) internal view returns(AuditTrail memory) {
+        require(requesterAuditTrails[requester][documentId].signatureAddress != address(0), "The audit for this document doesn't exists");
+
+        return requesterAuditTrails[requester][documentId];
+    }
+
+    function _checkExistence(address owner, string memory documentId) internal view returns(bool){
+        if(requesterAuditTrails[owner][documentId].signatureAddress != address(0)) return true;
+        else return false;
+    }
+
+    function _checkValidCaller(address userSmartContract) internal view {
+        require(
+            _validAddress(userSmartContract),
+            "Only Signaturit or a validated account can perform this action"
+        );
+    }
+
+    function _validAddress(address userSmartContract) internal view returns(bool){
+        address checkedAddress;
+        uint notificationIndex = 0;
+        bool result = false;
+
+        SignaturitUserInterface userContract = SignaturitUserInterface(userSmartContract);
+
+        if (admittedNotifiers[userContract.ownerAddress()][msg.sender]) {
+            result = true;
+
+        } else {
+            do {
+                checkedAddress = userContract.getAddressArrayAttribute(VALIDATED_NOTIFIERS_KEY, notificationIndex);
+
+                if (checkedAddress == msg.sender) {
+                    result = true;
+
+                    checkedAddress = address(0);
+                }
+
+                ++notificationIndex;
+            } while (checkedAddress != address(0));
+        }
+
+        return result;
     }
 }
--- contracts/interfaces/EventInterface.sol
+++ contracts/interfaces/EventInterface.sol
@@ -2,6 +2,8 @@ pragma solidity <0.6.0;
 
 
 contract EventInterface {
+    address public parent;
+    
     string public id;
     string public eventType;
     string public userAgent;
