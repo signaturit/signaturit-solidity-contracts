eff1532b296d004da18cb01e47c51957a4cf8347
Dario <dariolobuglio@gmail.com>
2020-03-26 16:47:02 +0100

    code review

--- contracts/UserAuthority.sol
+++ contracts/UserAuthority.sol
@@ -9,14 +9,26 @@ Gas to deploy: 907.423
 contract UserAuthority {
     address public rootAddress;
 
-    uint8 constant private USER_ROLE = 0;
-    uint8 constant private ADMIN_ROLE = 1;
-    uint8 constant private ROOT_ROLE = 2;
+    enum role {
+        UNDEFINED,
+        USER,
+        ADMIN,
+        ROOT
+    }
+
+    enum reputation {
+        UNDEFINED,
+        VERY_LOW,
+        LOW,
+        MEDIUM,
+        HIGH,
+        VERY_HIGH
+    }
 
     struct User {
         address contractAddress;
-        uint role;
-        uint reputation;
+        UserAuthority.role role;
+        UserAuthority.reputation reputation;
         bool validity;
         address[] managedUsers;
         mapping(address => bool) managers;
@@ -29,12 +41,12 @@ contract UserAuthority {
 
         address[] memory tmpAddresses;
 
-        users[rootAddress] = User(address(0), ROOT_ROLE, 5, true, tmpAddresses);
+        users[rootAddress] = User(address(0), role.ROOT, reputation.VERY_HIGH, true, tmpAddresses);
     }
 
     modifier allowedAdminCreation() {
         require(
-            users[msg.sender].role >= ADMIN_ROLE,
+            users[msg.sender].role >= role.ADMIN,
             "The account can't perform this action"
         );
 
@@ -43,7 +55,7 @@ contract UserAuthority {
 
     modifier allowedUserCreation() {
         require(
-            users[msg.sender].role > USER_ROLE,
+            users[msg.sender].role > role.USER,
             "The account can't perform this action"
         );
 
@@ -52,9 +64,17 @@ contract UserAuthority {
 
     modifier allowedEdit(address userAdr) {
         require(
-            users[msg.sender].role > USER_ROLE &&
+            users[msg.sender].role > role.USER &&
             isUserManager(userAdr, msg.sender),
             "Unauthorized action"
+        );
+
+        _;
+    }
+
+    modifier notCreatedYet(address userAdr) {
+        require(
+            uint8(users[userAdr].role) == 0, "User already exists"
         );
 
         _;
@@ -65,14 +85,9 @@ contract UserAuthority {
     )
         public
         allowedAdminCreation
+        notCreatedYet(adminAdr)
     {
-        address[] memory tmpAddresses;
-
-        users[adminAdr] = User(address(0), ADMIN_ROLE, 3, true, tmpAddresses);
-
-        users[adminAdr].managers[msg.sender] = true;
-
-        users[msg.sender].managedUsers.push(adminAdr);
+        _createUser(msg.sender, adminAdr, UserAuthority.role.ADMIN);
     }
 
     function createUser(
@@ -80,14 +95,9 @@ contract UserAuthority {
     )
         public
         allowedUserCreation
+        notCreatedYet(userAdr)
     {
-        address[] memory tmpAddresses;
-
-        users[userAdr] = User(address(0), USER_ROLE, 3, true, tmpAddresses);
-
-        users[userAdr].managers[msg.sender] = true;
-
-        users[msg.sender].managedUsers.push(userAdr);
+        _createUser(msg.sender, userAdr, UserAuthority.role.USER);
     }
 
     function createAdminAndUser(
@@ -96,20 +106,11 @@ contract UserAuthority {
     )
         public
         allowedAdminCreation
+        notCreatedYet(userAdr)
+        notCreatedYet(adminAdr)
     {
-        address[] memory tmpAddresses;
-
-        users[adminAdr] = User(address(0), ADMIN_ROLE, 3, true, tmpAddresses);
-
-        users[userAdr] = User(address(0), USER_ROLE, 3, true, tmpAddresses);
-
-        users[adminAdr].managers[msg.sender] = true;
-
-        users[userAdr].managers[adminAdr] = true;
-
-        users[msg.sender].managedUsers.push(adminAdr);
-
-        users[adminAdr].managedUsers.push(userAdr);
+        _createUser(msg.sender, adminAdr, UserAuthority.role.ADMIN);
+        _createUser(adminAdr, userAdr, UserAuthority.role.USER);
     }
 
     function setUserContract(
@@ -124,24 +125,24 @@ contract UserAuthority {
 
     function setRole(
         address userAdr,
-        uint role
+        UserAuthority.role inputRole
     )
         public
         allowedEdit(userAdr)
     {
-        require(role <= users[msg.sender].role, "Can't set an higher role");
+        require(uint8(inputRole) <= uint8(users[msg.sender].role), "Can't set an higher role");
 
-        users[userAdr].role = role;
+        users[userAdr].role = inputRole;
     }
 
     function setReputation(
         address userAdr,
-        uint reputation
+        UserAuthority.reputation inputReputation
     )
         public
         allowedEdit(userAdr)
     {
-        users[userAdr].reputation = reputation;
+        users[userAdr].reputation = inputReputation;
     }
 
     function setValidity(
@@ -161,8 +162,8 @@ contract UserAuthority {
         view
         returns(
             address contractAddress,
-            uint role,
-            uint reputation,
+            UserAuthority.role outputRole,
+            UserAuthority.reputation outputReputation,
             bool validity,
             uint managedUsers
         )
@@ -178,7 +179,7 @@ contract UserAuthority {
             );
 
         else
-            return(address(0), 0, 0, false, 0);
+            return(address(0), role.UNDEFINED, reputation.UNDEFINED, false, 0);
     }
 
     function getManagedUser(
@@ -218,4 +219,19 @@ contract UserAuthority {
     {
         return users[userAdr].managers[managerAdr];
     }
+
+    function _createUser(
+        address manager,
+        address user,
+        UserAuthority.role inputRole
+    ) internal {
+        address[] memory tmpAddresses;
+
+        users[user] = User(address(0), inputRole, reputation.MEDIUM, true, tmpAddresses);
+
+        users[user].managers[manager] = true;
+
+        users[manager].managedUsers.push(user);
+    }
+
 }
